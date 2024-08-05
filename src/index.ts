73341443892d719c797625dfd6a11ed2b99656d1
Jose Zerpa <zjose88@gmail.com>
2024-08-04 21:03:19 -0400

    Add uuid

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayEvent } from "aws-lambda";
+import { v4 as uuidv4 } from 'uuid';
 
 const client = new DynamoDBClient({ region: "us-east-1" });
 const docClient = DynamoDBDocumentClient.from(client);
   
 export const handler = async (event: APIGatewayEvent) => {
-  console.log(event);
   try {
-    const { title, description, status } = JSON.parse(event.body ?? "{}");
-    const uniqueId = "2";
+    if(!event.body) {
+      throw new Error("Missing request body");
+    }
+
+    const { title, description, status } = JSON.parse(event.body);
     const input = {
       "TableName": "Task",
       "Item": {
         "taskId": {
-          "S": uniqueId,
+          "S": uuidv4(),
         },
         "title": {
           "S": title,
