c810625750ab5073fe6d31f57a9353a1a528068d
Jose Zerpa <zjose88@gmail.com>
2024-08-11 00:54:41 -0400

    Refactor lambda index file

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,9 @@
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { APIGatewayEvent } from "aws-lambda";
-import { v4 as uuidv4 } from 'uuid';
-
-const client = new DynamoDBClient({ region: "us-east-1" });
-const docClient = DynamoDBDocumentClient.from(client);
+import { router } from "./business-logic/router";
   
 export const handler = async (event: APIGatewayEvent) => {
   try {
-    if(!event.body) {
-      throw new Error("Missing request body");
-    }
-
-    const { title, description, status } = JSON.parse(event.body);
-    const input = {
-      "TableName": "Task",
-      "Item": {
-        "taskId": {
-          "S": uuidv4(),
-        },
-        "title": {
-          "S": title,
-        },
-        "description": {
-          "S": description,
-        },
-        "status": {
-          "S": status,
-        },
-      },
-    };
-    
-    const command = new PutItemCommand(input);
-    const result = await docClient.send(command);
-  
+    const result = await router(event);
     const response = {
       statusCode: 200,
       body: JSON.stringify(result),
