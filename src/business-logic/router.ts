5a0c1541cc36ccdf3a5467388cc3ecbd71a440e0
Jose Zerpa <zjose88@gmail.com>
2024-08-11 00:53:01 -0400

    Add router

---
+++ src/business-logic/router.ts
@@ -0,0 +1,47 @@
+import { APIGatewayEvent } from "aws-lambda";
+import { createTask } from "./task/create";
+import { indexTask } from "./task/index";
+
+export async function router(event: APIGatewayEvent) {
+  if(!event.path) {
+    throw new Error("Missing request path");
+  }
+
+  const { httpMethod, path } = event;
+
+  const paths = path.split("/");
+  const resource = paths[1];
+  const parameter = paths[2];
+
+  if (resource === "task") {
+    if (parameter !== undefined) {
+      switch (httpMethod) {
+        case "POST":
+          return createTask(event);
+        case "GET":
+          return indexTask();
+        default:
+          throw new Error("Invalid method");
+      }
+    }
+    // else {
+    //   switch (httpMethod) {
+    //     case "GET":
+    //       return getTask(event);
+    //     case "PUT":
+    //       return updateTask(event);
+    //     case "DELETE":
+    //       return deleteTask(event);
+    //     default:
+    //       throw new Error("Invalid method");
+    //   }
+    // }
+  }
+}
+
+// Task
+// createTask (POST) /task { title, description, status }
+// getTask (GET) /task/1
+// indexTasks (GET) /task
+// updateTask (PUT) /task/1 { title, description, status }
+// deleteTask (DELETE) /task/1
