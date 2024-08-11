8be8002351bb8715c0e78bc1e6e6c88fe9011e8e
Jose Zerpa <zjose88@gmail.com>
2024-08-11 00:53:22 -0400

    Add create taks logic

---
+++ src/business-logic/task/create.ts
@@ -0,0 +1,33 @@
+import { APIGatewayEvent } from "aws-lambda";
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { documentClient } from "../../config/dynamodb";
+import { v4 as uuidv4 } from 'uuid';
+
+export async function createTask(event: APIGatewayEvent) {
+  if(!event.body) {
+    throw new Error("Missing request body");
+  }
+
+  const { title, description, status } = JSON.parse(event.body);
+  const input = {
+    "TableName": "Task",
+    "Item": {
+      "taskId": {
+        "S": uuidv4(),
+      },
+      "title": {
+        "S": title,
+      },
+      "description": {
+        "S": description,
+      },
+      "status": {
+        "S": status,
+      },
+    },
+  };
+  
+  const command = new PutItemCommand(input);
+  const result = await documentClient.send(command);
+  return result;
+}
